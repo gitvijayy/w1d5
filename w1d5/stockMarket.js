78ea863193406f8b603a7ec36c4d955dc730a73f
Vijay <7822vijay@gmail.com>
2019-05-04 08:34:07 +0000

    max profit brute force

--- w1d5/stockMarket.js
+++ w1d5/stockMarket.js
@@ -1,22 +1,19 @@
 function maxProfit(stockPrice) {
-var arr = [];
-var arr2 = [];
+var price = [];
+var profit = [];
 
 for(var i = 0; i < stockPrice.length; i++) {
   for(var j = i + 1; j < stockPrice.length; j++) {
 
-    arr.push([stockPrice[i],stockPrice[j]]);
-    arr2.push(Number(stockPrice[j] - stockPrice[i]));
+    price.push([stockPrice[i],stockPrice[j]]);
+    profit.push(Number(stockPrice[j] - stockPrice[i]));
   };
 };
 
-var prices = arr[arr2.indexOf(Math.max(...arr2))];
-return Math.max(...arr2) > 0 ?
+var prices = price[profit.indexOf(Math.max(...profit))];
+return Math.max(...profit) > 0 ?
 "If you buy at $" + prices[0] + " and sell at $" + prices[1] +
 " you attain the maximum profit for the day: $" + (prices[1] - prices[0])
 : -1;
 
 }
-
-
-console.log(maxProfit([45,44,31,4,3,2]));
